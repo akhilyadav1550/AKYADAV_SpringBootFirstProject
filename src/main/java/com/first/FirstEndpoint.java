75415aef5a7f13899ea25e763878872a45e43093
AKYADAV <akyadav@desktop.com>
2025-12-05 01:08:14 +0530

    initial commit

---
+++ src/main/java/com/first/FirstEndpoint.java
@@ -0,0 +1,16 @@
+package com.first;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstEndpoint {
+
+	@GetMapping({ "/", "/hello" })
+	public String HelloWorlAPI() {
+		return "Hello World from AKYADAV's First SpringBoot Project....";
+	}
+	
+	//Open the browser and open the "http://localhost:8080/" or "http://localhost:8080/hello"
+
+}
